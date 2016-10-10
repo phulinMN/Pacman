721b6bf0871ec6faccf0cbf8d524c83fe0b7a483
phulin <pongpaiMN@gmail.com>
2016-10-11 00:44:28 +0700

    dis pacman

--- desktop/src/com/phulin/game/desktop/DesktopLauncher.java
+++ desktop/src/com/phulin/game/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.phulin.game.PacmanGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PacmanGame(), config);
-	}
+	 public static void main (String[] arg) {
+	       LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	       config.width = 800;
+	       config.height = 600;
+	       new LwjglApplication(new PacmanGame(), config);
+	  }
 }
